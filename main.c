e1193208b3af4af5ce699c66510b3ec962445138
João Brentano <joaobrentano.br@gmail.com>
2020-10-01 19:11:58 -0300

    converter para cinza

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ typedef struct {
 
 // Protótipos
 void load(char* name, Img* pic);
+void foo(Img* pic);
 
 // Carrega uma imagem para a struct Img
 void load(char* name, Img* pic)
@@ -32,20 +33,49 @@ void load(char* name, Img* pic)
     printf("Load: %d x %d x %d\n", pic->width, pic->height, chan);
 }
 
+void foo(Img* pic)
+{
+    int ns = pic->width * pic->height / 20;
+    RGB newpixels[ns];
+}
+
 int main(int argc, char** argv)
 {
     Img pic;
-    if(argc < 1) {
+    if(argc == 1) {
         printf("loader [img]\n");
         exit(1);
     }
     load(argv[1], &pic);
 
-    printf("Primeiros 10 pixels da imagem:\n");
-    for(int i=0; i<10; i++) {
-        printf("[%02X %02X %02X] ", pic.img[i].r, pic.img[i].g, pic.img[i].b);
+    for(int tam=(pic.width)*(pic.height)-1; tam>=0; tam--) {
+        unsigned char cinza = (0.3 * pic.img[tam].r + 0.59 * pic.img[tam].g + 0.11 * pic.img[tam].b);
+        pic.img[tam].r = cinza;
+        pic.img[tam].g = cinza;
+        pic.img[tam].b = cinza;
+        printf("[%02X %02X %02X] ", pic.img[tam].r, pic.img[tam].g, pic.img[tam].b); 
     }
-    printf("\n");
 
+    // Exemplo: gravando um arquivo de saída com a imagem (não é necessário para o trabalho, apenas
+    // para ver o resultado intermediário, por ex, da conversão para tons de cinza)
+    SOIL_save_image("out.bmp", SOIL_SAVE_TYPE_BMP, pic.width, pic.height,
+        3, (const unsigned char*) pic.img);
+
+    // Exemplo: gravando um arquivo saida.html
+    FILE* arq = fopen("saida.html", "w"); // criar o arquivo: w
+    if(arq == NULL) // 0, falso
+    {
+        printf("Erro abrindo arquivo de saída\n");
+        exit(1);
+    }
+
+    fprintf(arq, "<html>\n");
+    fprintf(arq,"<head>\n");
+    fprintf(arq,"</head>\n");
+    fprintf(arq,"<body>\n");
+    fprintf(arq,"<h1>Oi!</h1>\n");
+    fprintf(arq,"</body>\n");
+
+    fclose(arq);
     free(pic.img);
 }
